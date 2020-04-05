87b77b08306c1fb4556d9986b4389a7b090891dc
ja <s18264@pjwstk.edu.pl>
2020-04-05 08:46:43 +0200

    Pierwszy commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var httpClient = new HttpClient();
+            Console.WriteLine("podaj input");
+            //var input = Console.ReadLine();
+
+            var response = await httpClient.GetAsync(args[0]);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var html = await response.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-zA-Z0-9]+@[a-zA-Z.]+");
+
+                MatchCollection matches = regex.Matches(html);
+                foreach(var i in matches)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            
+
+            Console.WriteLine("Koniec!");
         }
     }
 }
