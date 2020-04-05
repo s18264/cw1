70841793915bc3b9632e10f16ee775633e4498a3
ja <s18264@pjwstk.edu.pl>
2020-04-05 09:30:06 +0200

    rozbudowanie

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,8 +9,23 @@ namespace ConsoleApp1
     {
         public static async Task Main(string[] args)
         {
+            if(args.Length == 0)
+            {
+                throw new ArgumentNullException("nie podano argumentu");
+            }
+            
+            Uri uriResult;
+            bool result = Uri.TryCreate(args[0], UriKind.Absolute, out uriResult)
+                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+
+            if(result == false)
+            {
+                throw new ArgumentException("niepoprawny url");
+            }
+
+
             var httpClient = new HttpClient();
-            Console.WriteLine("podaj input");
+            //Console.WriteLine("podaj input");
             //var input = Console.ReadLine();
 
             var response = await httpClient.GetAsync(args[0]);
@@ -21,14 +36,20 @@ namespace ConsoleApp1
                 var regex = new Regex("[a-zA-Z0-9]+@[a-zA-Z.]+");
 
                 MatchCollection matches = regex.Matches(html);
+                if (matches.Count == 0)
+                {
+                    Console.WriteLine("Nie znaleziono adresów email");
+                }
+                else { 
                 foreach(var i in matches)
                 {
                     Console.WriteLine(i);
                 }
+                }
             }
 
-            
 
+            httpClient.Dispose();
             Console.WriteLine("Koniec!");
         }
     }
